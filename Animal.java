53fbcac62dc41c455499933363353b2380e9d7d0
Mohamed Nouaman <mnouaman@smb.sqli.com>
2022-03-19 19:51:36 +0100

    cat greeting implemetation

--- Animal.java
+++ Animal.java
@@ -3,10 +3,18 @@ public class Animal{
      public static String getDefaultGreeting(){
          return "Hello";
      }
+     public static String catGreeting() {
+               return "Meow";
+     }
      public static void main(String[] args) {
 
-       
-         System.out.println(getDefaultGreeting());
+         String firstArgumenet=args[0];
+
+         if(firstArgumenet.equalsIgnoreCase("cat")) System.out.println(catGreeting());
+
+        else  System.out.println(getDefaultGreeting());
+
+        
 
      }
 
