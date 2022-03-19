8d9aab40fd35598a8d2fb1b54c89c57685745550
Mohamed Nouaman <mnouaman@smb.sqli.com>
2022-03-19 16:43:53 +0100

    default implementation

---
+++ Animal.java
@@ -0,0 +1,13 @@
+public class Animal{
+
+     public static String getDefaultGreeting(){
+         return "Hello";
+     }
+     public static void main(String[] args) {
+
+       
+         System.out.println(getDefaultGreeting());
+
+     }
+
+}
