be2befdad8b0cd42062fd74e2bfcff159bafb60d
Mohamed Nouaman <mnouaman@smb.sqli.com>
2022-03-19 19:58:26 +0100

    bird greeeting implementation

--- Animal.java
+++ Animal.java
@@ -6,12 +6,15 @@ public class Animal{
      public static String catGreeting() {
                return "Meow";
      }
+     public static String birdGreeting(){
+         return "Tweet";
+     }
      public static void main(String[] args) {
 
          String firstArgumenet=args[0];
 
-         if(firstArgumenet.equalsIgnoreCase("cat")) System.out.println(catGreeting());
-
+        if(firstArgumenet.equalsIgnoreCase("cat")) System.out.println(catGreeting());
+        else if(firstArgumenet.equalsIgnoreCase("bird")) System.out.println(birdGreeting());
         else  System.out.println(getDefaultGreeting());
 
         
